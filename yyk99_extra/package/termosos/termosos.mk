893f0f41f20bf7cc45f76bc9421715674af8a5d8
yyk <yyk@Ubuntu.myguest.virtualbox.org>
2026-02-02 12:01:14 -0500

    Update driver and dot.config
    Add strace utility

--- yyk99_extra/package/termosos/termosos.mk
+++ yyk99_extra/package/termosos/termosos.mk
@@ -4,8 +4,8 @@ TERMOSOS_SITE_METHOD = local
 TERMOSOS_LICENSE = MIT
 
 # For kernel modules
-#MYMODULE_MODULE_SUBDIRS = .
-#MYMODULE_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+TERMOSOS_MODULE_SUBDIRS = driver
+#TERMOSOS_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 define TERMOSOS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
@@ -14,9 +14,10 @@ endef
 define TERMOSOS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/udpserver/udpserver $(TARGET_DIR)/usr/sbin
 	$(INSTALL) -D -m 0755 $(@D)/udpserver/udp_echo_client $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/driver/S98dht11_driver $(TARGET_DIR)/etc/init.d
 endef
 
-#$(eval $(kernel-module))
+$(eval $(kernel-module))
 $(eval $(generic-package))
 
 
